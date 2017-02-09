9d691aa6e7950b8ed8a6d6ce110661623279c367
Yangsou <namnguyentrung95@gmail.com>
2017-02-10 00:54:32 +0700

    upload sidebar main

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
   $('.header_search_btn-categories').on('click', function(){
     $('.form_menu-cate').toggleClass("active");
   });
+
+  //tab in index
+  $('.tabs_btn').on('click', function(){
+    //get name tab
+    var _tab = $(this).data('tab');
+    //active tab_btn click
+    $(this).parent().find('.tabs_btn').removeClass('active');
+    $(this).addClass('active');
+    //show tabs-content_wrap
+    $(this).parent().parent().find('.tabs-content_wrap').removeClass('active');
+    $('#' + _tab).addClass('active');
+  })
 });
